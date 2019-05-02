3e98d3088fea57d131f5ce206b37ab62c1cf4366
Demin Dmitriy <demindf@gmail.com>
2019-05-02 07:35:24 +0300

    add basic task structures; implement reading the input; add Timer

--- src/main.cpp
+++ src/main.cpp
@@ -1,10 +1,121 @@
-#pragma GCC optimize ("O3")
+#ifdef ONLINE_JUDGE
+    #pragma GCC optimize ("O3")
+    #define NDEBUG
+#endif
 
+
+#include <fstream>
 #include <iostream>
+#include <vector>
+#include <iomanip>
+
+#include <ctime>
+#include <cassert>
 
 
-int main()
+struct Vector
 {
-	std::ios::sync_with_stdio(false);
-	std::cout << "hello\n";
+    int x;
+    int y;
+
+    int l1_norm() const
+    {
+        return abs(x) + abs(y);
+    }
+};
+
+Vector operator-(Vector a, Vector b)
+{
+    return Vector
+    {
+        .x = a.x - b.x,
+        .y = a.y - b.y
+    };
+}
+
+int distance(Vector a, Vector b)
+{
+    return (a - b).l1_norm();
+}
+
+struct TimeWindow
+{
+    int from;
+    int to;
+};
+
+struct Location
+{
+    Vector point;
+    int duration;
+    int p;
+    TimeWindow time_window;
+
+    static Location read(std::istream& in)
+    {
+        Location result;
+        in >> result.point.x
+           >> result.point.y
+           >> result.duration
+           >> result.p
+           >> result.time_window.from
+           >> result.time_window.to;
+        return result;
+    }
+};
+
+struct Task
+{
+    int n;
+    std::vector<Location> locations;
+
+    void read(std::istream& in)
+    {
+        assert(locations.empty());
+
+        in >> n;
+        locations.reserve(n);
+        for (int i = 0; i != n; ++i)
+        {
+            locations.emplace_back(Location::read(in));
+        }
+    }
+};
+
+struct Timer
+{
+    clock_t start_time;
+
+    Timer()
+        : start_time(clock())
+    { }
+
+    double seconds_elapsed() const
+    {
+        return double(clock() - start_time) / CLOCKS_PER_SEC;
+    }
+};
+
+
+
+Task task;
+const Timer timer;
+constexpr double TIME_LIMIT_SECONDS = 15;
+
+int main(int argc, char** argv)
+{
+    std::ios::sync_with_stdio(false);
+    std::cin.tie(nullptr);
+
+    #ifdef READ_TASK_FROM_FILE
+        assert(argc == 2);
+        {
+            std::ifstream input(argv[1]);
+            task.read(input);
+        }
+    #else
+        task.read(std::cin);
+    #endif
+
+    std::cerr << "Elapsed: " << std::setprecision(2) << std::fixed << timer.seconds_elapsed() << " seconds.\n";
 }
